579302a46aa331c1ecf5b834c4c15c82b664b5db
mizan4392 <md.mizan4392@mail.com>
2020-05-12 15:43:03 +0600

    added auth middleware

--- server/server.js
+++ server/server.js
@@ -17,11 +17,27 @@ app.use(cookiParser())
 
 //Models
 const {User} = require('./models/user')
+//middlewares
+const {auth} = require('./middleware/auth')
 
 
 //======================
 //          USERS
 //======================
+
+app.get('/api/users/auth',auth,(req,res)=>{
+
+    res.status(200).json({
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth:true,
+        email:req.user.email,
+        name:req.user.name,
+        lastname:req.user.role,
+        cart:req.user.cart,
+        history:req.user.history
+    })
+
+})
 
 app.post('/api/users/register',(req,res)=>{
     const user = new User(req.body)
@@ -31,8 +47,7 @@ app.post('/api/users/register',(req,res)=>{
             return res.json({success:false,err})
         }else{
             res.status(200).json({
-                success:true,
-                userData:doc
+                success:true
             })
         }
     })
