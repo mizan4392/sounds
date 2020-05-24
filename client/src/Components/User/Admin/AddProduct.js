d407aba8d0a271358bc024368112d047deda91f2
mizan4392 <dev.mizan4392@gmail.com>
2020-05-24 17:07:33 +0600

    added addproduct form

--- client/src/Components/User/Admin/AddProduct.js
+++ client/src/Components/User/Admin/AddProduct.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import { TextField, Button ,TextareaAutosize} from '@material-ui/core'
+import UserLayout from '../../../hoc/UserLayout'
 
 class AddProduct extends Component {
+
+
+    state = {
+        product_name: '',
+        product_des: '',
+        price:0
+    }
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <UserLayout>
+                <h2>Add new Product</h2>
+                <div style={{ display: 'flex', flexDirection: "column" }}>
+                    <TextField type="text" name="product_name" value={this.state.product_name} placeholder="Enter Product Name" id="outlined-basic" label="ProductName" size='medium' onChange={this.handleInputChange} /><br></br>
+                    <TextareaAutosize rows={4} type="text" name="product_des" value={this.state.product_des} placeholder="Product Description" id="outlined-basic" label="description" size='medium' onChange={this.handleInputChange} /><br></br>
+                    <TextField type="number" name="price" value={this.state.price} placeholder="Enter Product Price" id="outlined-basic" label="Price" size='medium' onChange={this.handleInputChange} /><br></br>
+                    <div>
+                        product Brand
+                    </div>
+                    <div>
+                        Shiping
+                    </div>
+                    <div>
+                        Avilabel in Stock
+                    </div>
+                </div>
+            </UserLayout>
+
         )
     }
 }
 
-export default  AddProduct
+export default AddProduct
